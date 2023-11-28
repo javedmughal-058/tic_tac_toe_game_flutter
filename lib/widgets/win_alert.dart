2c61a618b81d4bde86fdca0d2ad1cfc308d7a4cd
javedmughal <javedmughal609@gmail.com>
2023-11-28 12:51:11 +0500

    make alert separate

--- lib/widgets/win_alert.dart
+++ lib/widgets/win_alert.dart
@@ -2,25 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tic_tac_toe_game_flutter/provider/game_provider.dart';
 
-class WinnerDialog extends StatelessWidget {
-  const WinnerDialog({Key? key}) : super(key: key);
+Widget winAlert(context, GameProvider gameProvider)=> AlertDialog(
+  title: const Text('Winner!'),
+  content: Text('${gameProvider.winPlayer} wins the game!'),
+  actions: [
+    TextButton(
+      onPressed: (){
+        gameProvider.clearBoard(context);
+        Navigator.of(context).pop();
+      },
+      child: const Text('OK', style: TextStyle(color: Colors.black)),
+    ),
+  ],
+);
 
-  @override
-  Widget build(BuildContext context) {
-    GameProvider gameProvider = Provider.of<GameProvider>(context);
-
-    return AlertDialog(
-      title: const Text('Winner!'),
-      content: Text('${gameProvider.winPlayer} wins the game!'),
-      actions: [
-        TextButton(
-          onPressed: () {
-            gameProvider.clearBoard(context);
-            Navigator.of(context).pop();
-          },
-          child: const Text('OK'),
-        ),
-      ],
-    );
-  }
-}
+Widget drawAlert(context, GameProvider gameProvider)=> AlertDialog(
+  backgroundColor: Colors.amber,
+  title: const Text('Draw!', style: TextStyle(color: Colors.white)),
+  content: const Text('No One Win!', style: TextStyle(color: Colors.white)),
+  actions: [
+    TextButton(
+      onPressed: (){
+        gameProvider.clearBoard(context);
+        Navigator.of(context).pop();
+      },
+      child: const Text('OK', style: TextStyle(color: Colors.white)),
+    ),
+  ],
+);
